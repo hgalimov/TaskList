1d1611a7f9fdf47609c609892a2be2c6382b1e47
Khalim Galimov <khalimgal@gmail.com>
2020-09-24 12:05:56 +0300

    Modified class TaskEntity

--- src/main/java/ru/uennar/birem/TaskEntity.java
+++ src/main/java/ru/uennar/birem/TaskEntity.java
@@ -1,4 +1,20 @@
 package ru.uennar.birem;
 
 public class TaskEntity {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskEntity{" +
+                "id=" + id +
+                '}';
+    }
 }
